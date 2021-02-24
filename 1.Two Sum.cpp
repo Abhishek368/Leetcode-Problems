3c549b43173a3cdf5f3a8a508fee4417d177d9c8
Abhishek368 <41854966+Abhishek368@users.noreply.github.com>
2021-02-24 22:09:26 +0530

    Create 1.Two Sum.cpp

---
+++ 1.Two Sum.cpp
@@ -0,0 +1,23 @@
+/*
+  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+  You can return the answer in any order.
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      unordered_map<int,int> umap;
+      for(int i=0;i<nums.size();i++){
+          umap[nums[i]] = i;
+      }
+      for(int i=0;i<nums.size();i++){
+          int diff = target - nums[i];
+          if(umap.count(diff) && umap[diff] != i){
+              return {i,umap[diff]};
+          }
+      }
+        return {};
+      
+    }
+};
